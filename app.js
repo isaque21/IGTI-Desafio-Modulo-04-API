ac60dafc2d0d52792b4e54131e8a3ee4771b026e
Isaque Alcantara <isaque21@gmail.com>
2020-07-12 12:23:34 -0300

    Criação de variável de ambiente para cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     // origin: "http://localhost:8080",
-    origin: "http://localhost:3001",
+    origin: process.env.ORIGIN_URL,
   })
 );
 
